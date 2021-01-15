6b9df298ad4b6b3f2db52c1d44e2ebd3c355163d
DespawnedDiamond <despawnedd@acrazytown.com>
2021-01-15 15:53:04 +0100

    yet even more pytest testing

--- tests/file_test.py
+++ tests/file_test.py
@@ -2,13 +2,13 @@
 import os
 
 def test_checkForLogsFolder():
-    if os.path.isdir("../source/data/user/temp/logs"):
+    if os.path.isdir("source/data/user/temp/logs"):
         assert True
     else:
         assert False
 
 def test_checkForAssetsFolder():
-    if os.path.isdir("../source/data/assets"):
+    if os.path.isdir("source/data/assets"):
         assert True
     else:
         assert False
