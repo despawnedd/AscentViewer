43237ba811e5570564921d5275c20f25975b3c1a
DespawnedDiamond <despawnedd@acrazytown.com>
2021-01-15 15:50:46 +0100

    more.. more testing

--- tests/file_test.py
+++ tests/file_test.py
@@ -1,9 +1,14 @@
+# https://www.geeksforgeeks.org/python-check-if-a-file-or-directory-exists-2/
 import os
 
-def checkForLogsFolder():
-    if os.path.exists("source/data/user/logs"):
+def test_checkForLogsFolder():
+    if os.path.isdir("../source/data/user/temp/logs"):
         assert True
+    else:
+        assert False
 
-def checkForAssetsFolder():
-    if os.path.exists("source/data/assets"):
+def test_checkForAssetsFolder():
+    if os.path.isdir("../source/data/assets"):
         assert True
+    else:
+        assert False
